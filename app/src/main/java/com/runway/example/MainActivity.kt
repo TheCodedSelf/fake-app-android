338a8a384d6b52feac2b4a43c931e9348734fc4f
Alessandro Martin <martin.acg@gmail.com>
2023-01-26 16:32:55 +0000

    Added comment to main activity

--- app/src/main/java/com/runway/example/MainActivity.kt
+++ app/src/main/java/com/runway/example/MainActivity.kt
@@ -3,6 +3,7 @@ package com.runway.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// This is the app's main activity (DUH!)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
