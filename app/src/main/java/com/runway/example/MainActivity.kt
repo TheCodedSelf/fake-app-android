0d619cec93c5b3b90c99c7302b62ad3b1929df5c
Alessandro Martin <martin.acg@gmail.com>
2023-02-13 19:39:07 +0000

    Activity comment for dual tag

--- app/src/main/java/com/runway/example/MainActivity.kt
+++ app/src/main/java/com/runway/example/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // This is the app's main activity and there's no other like it!
+// Another line of comments for dual tagging
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
