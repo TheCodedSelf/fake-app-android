ddc529aa0e864e21860136a292d4976e08c70682
Alessandro Martin <martin.acg@gmail.com>
2023-02-24 17:49:15 +0000

    First commit

--- app/src/main/java/com/runway/example/MainActivity.kt
+++ app/src/main/java/com/runway/example/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 // Yet another line!
 // Let's add another line
 // Another line to test squashing
+// Now one more time
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
